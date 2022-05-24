ded8b74902def531ef46d9c24f0a67b8f0993196
Adrian Czyż <adicz5200@gmail.com>
2022-05-24 17:58:26 +0200

    OSA-2 added user roles and web configuration

---
+++ src/main/java/pl/sda/shop/onlineshop/security/SecurityDetails.java
@@ -0,0 +1,18 @@
+package pl.sda.shop.onlineshop.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityDetails {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
---
+++ src/main/java/pl/sda/shop/onlineshop/security/WebConfiguration.java
@@ -0,0 +1,44 @@
+package pl.sda.shop.onlineshop.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import pl.sda.shop.onlineshop.service.UserService;
+
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests()
+                .antMatchers("/**").denyAll()
+                //NON-LOGGED USERS
+                .antMatchers(HttpMethod.POST, "/auth/signup").permitAll()
+                //LOGGED USER
+                .antMatchers(HttpMethod.GET, "/auth/current").fullyAuthenticated()
+                .antMatchers(HttpMethod.PATCH, "/user/{id}").hasAnyRole("USER", "ADMIN")
+                // ADMIN METHODS
+                .antMatchers("/user/*").hasRole("ADMIN");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder);
+    }
+}
