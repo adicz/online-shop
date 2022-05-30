83af612023b86df9d2e2438127898706a1e64054
mischa95 <88509830+mischa95@users.noreply.github.com>
2022-05-30 08:15:55 +0200

    Revert "Osa 4 product model"

--- src/main/java/pl/sda/shop/onlineshop/exception/category/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package pl.sda.shop.onlineshop.exception.category;
-
-public class CategoryNotFoundException extends RuntimeException{
-
-    public CategoryNotFoundException(String message){
-        super(message);
-    }
-}
--- src/main/java/pl/sda/shop/onlineshop/model/User.java
+++
@@ -1,8 +0,0 @@
-package pl.sda.shop.onlineshop.model;
-
-public class User {
-
-    public Long id;
-    public String username;
-    public String password;
-}
