db59bcb514c893e5c78fe430ca92f1c5e37f4a56
Adrian Czyż <adicz5200@gmail.com>
2022-07-21 14:21:54 +0200

    Deploy

--- src/test/java/pl/sda/shop/onlineshop/service/CategoryServiceTest.java
+++ src/test/java/pl/sda/shop/onlineshop/service/CategoryServiceTest.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
